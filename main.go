da11366e63b6bca8866294afa60aa1f5555f9933
kunwardeep <kunwardeep@gmail.com>
2019-09-06 16:26:28 +1000

    setup a working webserver with a grpc call

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
+
+func makeGrpcCall(userName string) string {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := userName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	msg := fmt.Sprintf("Greeting: %s", r.GetMessage())
+	log.Printf(msg)
+	return msg
+}
+
+func main() {
+	http.HandleFunc("/", echoString)
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+
+		fmt.Println(name)
+		type data struct {
+			Greeting string
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data{Greeting: makeGrpcCall(name)})
+	})
+
+	log.Fatal(http.ListenAndServe(":9999", nil))
+
+}
